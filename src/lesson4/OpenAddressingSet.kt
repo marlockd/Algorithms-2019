750ba875eb4ce985dac0a6f7d9bfd049186a9d54
marlockd <denislogvinov42@gmail.com>
2020-02-21 03:17:04 +0300

    4 lesson + tests

--- src/lesson4/OpenAddressingSet.kt
+++ src/lesson4/OpenAddressingSet.kt
@@ -1,6 +1,9 @@
 package lesson4
 
+import java.lang.StringBuilder
+
 class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>() {
+
     init {
         require(bits in 2..31)
     }
@@ -9,17 +12,23 @@ class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>(
 
     private val storage = Array<Any?>(capacity) { null }
 
+    private val removed = Array(capacity) { false }
+
     override var size: Int = 0
+
+    private var firstIndex: Int = 0
 
     private fun T.startingIndex(): Int {
         return hashCode() and (0x7FFFFFFF shr (31 - bits))
     }
 
+    //трудоемкость - О(1)
+    //ресурсоемкость - О(capacity)
     override fun contains(element: T): Boolean {
         var index = element.startingIndex()
         var current = storage[index]
         while (current != null) {
-            if (current == element) {
+            if (current == element && !removed[index]) {
                 return true
             }
             index = (index + 1) % capacity
@@ -28,12 +37,14 @@ class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>(
         return false
     }
 
+    //трудоемкость - О(1)
+    //ресурсоемкость - О(capacity)
     override fun add(element: T): Boolean {
         val startingIndex = element.startingIndex()
         var index = startingIndex
         var current = storage[index]
         while (current != null) {
-            if (current == element) {
+            if (current == element && !removed[index]) {
                 return false
             }
             index = (index + 1) % capacity
@@ -42,20 +53,131 @@ class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>(
         }
         storage[index] = element
         size++
+        if (size == 1) {
+            firstIndex = index
+        }
         return true
     }
 
     /**
      * Для этой задачи пока нет тестов, но вы можете попробовать привести решение и добавить к нему тесты
      */
+    //трудоемкость - О(1)
+    //ресурсоемкость - О(capacity)
     override fun remove(element: T): Boolean {
-        TODO("not implemented")
+        var index = element.startingIndex()
+        var current = storage[index]
+        while (current != null) {
+            if (current == element) {
+                removed[index] = true
+                size--
+                return true
+            }
+            index = (index + 1) % capacity
+            current = storage[index]
+        }
+        return false
     }
 
     /**
      * Для этой задачи пока нет тестов, но вы можете попробовать привести решение и добавить к нему тесты
      */
     override fun iterator(): MutableIterator<T> {
-        TODO("not implemented")
+        return OpenAddressingIterator()
     }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as OpenAddressingSet<*>
+
+        if (bits != other.bits) return false
+        if (capacity != other.capacity) return false
+        if (!storage.contentEquals(other.storage)) return false
+        if (!removed.contentEquals(other.removed)) return false
+        if (size != other.size) return false
+        if (firstIndex != other.firstIndex) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + bits
+        result = 31 * result + capacity
+        result = 31 * result + storage.contentHashCode()
+        result = 31 * result + removed.contentHashCode()
+        result = 31 * result + size
+        result = 31 * result + firstIndex
+        return result
+    }
+
+    //трудоемкость - О(1)
+    //ресурсоемкость - О(capacity)
+    override fun toString(): String {
+        if (size == 0) return emptyArray<String>().toString()
+        val sb = StringBuilder()
+        sb.append("[")
+        val iter = iterator()
+        while (iter.hasNext()) {
+            sb.append(" ${iter.next()},")
+        }
+        sb.deleteCharAt(sb.lastIndex)
+        sb.append(" ]")
+        return sb.toString()
+    }
+
+    override fun isEmpty(): Boolean {
+        return size == 0
+    }
+
+    private inner class OpenAddressingIterator<T> internal constructor() : MutableIterator<T> {
+        private var currentIndex: Int = 0
+        private var currentElement: Any? = null
+
+        init {
+            if (size > 0) {
+                currentIndex = firstIndex
+                currentElement = storage[firstIndex]
+            }
+        }
+
+        //трудоемкость - О(1)
+        //ресурсоемкость - О(1)
+        override fun hasNext(): Boolean {
+            if (currentIndex < 0 || currentIndex >= capacity) {
+                currentIndex = 0
+                return false
+            }
+            return !removed[currentIndex] && currentElement != null
+        }
+
+        //трудоемкость - О(1)
+        //ресурсоемкость - О(1)
+        override fun next(): T {
+            var index = currentIndex
+            val result = currentElement
+            index = (index + 1) % capacity
+            currentElement = storage[index]
+            currentIndex = index
+            return result as T
+        }
+
+
+        //трудоемкость - О(1)
+        //ресурсоемкость - О(1)
+        override fun remove() {
+            removed[currentIndex] = true
+            currentIndex = (currentIndex - 1) % capacity
+            if (size > 0) {
+                size--
+            }
+        }
+
+    }
+
+
 }
--- test/lesson4/OpenAddressingSetTest.kt
+++ test/lesson4/OpenAddressingSetTest.kt
@@ -20,4 +20,71 @@ class OpenAddressingSetTest {
         assertFalse("Gamma" in set)
         assertTrue("Omega" in set)
     }
+
+    @Test
+    fun contains() {
+        val set = OpenAddressingSet<String>(16)
+        assertTrue(set.isEmpty())
+        set.add("Alpha")
+        set.add("Beta")
+        set.add("Omega")
+        assertSame(3, set.size)
+        assertTrue("Beta" in set)
+        assertFalse("Gamma" in set)
+        assertTrue("Omega" in set)
+
+    }
+
+    @Test
+    fun remove() {
+        val set = OpenAddressingSet<String>(16)
+        set.add("Alpha")
+        set.add("Beta")
+        set.add("Omega")
+        set.remove("Omega")
+        set.remove("Alpha")
+        assertSame(1, set.size)
+        assertTrue("Beta" in set)
+        assertFalse("Alpha" in set)
+        assertFalse("Omega" in set)
+        set.remove("Beta")
+        assertTrue(set.isEmpty())
+    }
+
+    @Test
+    fun iterator() {
+        val set = OpenAddressingSet<String>(16)
+        set.add("A")
+        set.add("B")
+        set.add("C")
+        set.add("D")
+        var it = set.iterator()
+        var count = 0
+        val expected = arrayOf("A", "B", "C", "D")
+        val actual = arrayOfNulls<String>(4)
+        while (it.hasNext()) {
+            actual[count] = it.next()
+            count++
+        }
+        assertEquals(4, count)
+        assertArrayEquals(expected, actual)
+        it = set.iterator()
+        while (it.hasNext()) {
+            it.remove()
+        }
+        assertTrue(set.isEmpty())
+    }
+
+    @Test
+    fun toStringTest() {
+        val set = OpenAddressingSet<String>(16)
+        set.add("A")
+        set.add("B")
+        set.add("C")
+        set.add("D")
+        set.remove("D")
+        val actual = "[ A, B, C ]"
+        assertEquals(set.toString(), actual)
+    }
+
 }
